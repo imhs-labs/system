5e307dc974eb6eabde0e01c72688368d08916baf
Bradley Watton <bradley.watton@icloud.com>
2024-04-01 09:00:13 +0100

    fix Button props

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import { Button as BaseButton } from 'govuk-react'
 import { theme } from './theme';
+import { ComponentPropsWithoutRef } from 'react'
 
-export const Button: React.FC<HTMLButtonElement> = styled(BaseButton)({
+export const Button: React.FC<ComponentPropsWithoutRef<'button'>> = styled(BaseButton)({
   backgroundColor: theme.colors.primary,
   '&:hover, &:focus': {
     backgroundColor: theme.colors.primaryDark,
