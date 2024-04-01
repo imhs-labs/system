376887a45bd46065076effc2b282367d17a838e5
Bradley Watton <bradley.watton@icloud.com>
2024-04-01 08:48:15 +0100

    tsc fix

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Button as BaseButton } from 'govuk-react'
 import { theme } from './theme';
 
-export const Button = styled(BaseButton)({
+export const Button: React.FC<HTMLButtonElement> = styled(BaseButton)({
   backgroundColor: theme.colors.primary,
   '&:hover, &:focus': {
     backgroundColor: theme.colors.primaryDark,
